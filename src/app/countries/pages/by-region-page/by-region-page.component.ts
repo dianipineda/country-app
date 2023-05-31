ed6cbf6ca43b61c691a0f86591694a038dad5bd1
dianipineda <dianipineda55@gmail.com>
2023-05-31 14:51:01 -0500

    persistencia de datos en el service al cambio de ruta y recarga del navegador por medio de propiedades tipadas, ejecucion de metodos de local storage llamadas dentro del operador tap

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
-import { Country } from '../../interfaces/country';
-
-type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+import { Country } from '../../interfaces/country.interface';
+import { Region } from '../../interfaces/region.type';
 
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: [],
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
   public countries: Country[] = [];
   public regions: Region[] = [
     'Africa',
@@ -19,7 +18,13 @@ export class ByRegionPageComponent {
     'Oceania',
   ];
   public selectedRegion?: Region;
+
   constructor(private countriesService: CountriesService) {}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
 
   searchByRegion(region: Region): void {
     this.selectedRegion = region;
